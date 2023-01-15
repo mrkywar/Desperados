81dfe04b93cad70b2accb0236cc7eaffe4c80399
mykywar <mr.kywar@gmail.com>
2023-01-16 00:03:19 +0100

    [WIP] ZombieTrait is working

--- desperados.game.php
+++ desperados.game.php
@@ -1,9 +1,13 @@
 <?php
 
+use Desperados\Game\ZombieTrait;
+
 $swdNamespaceAutoload = function ($class) {
     $classParts = explode('\\', $class);
+    
     if ($classParts[0] == 'Desperados') {
         array_shift($classParts);
+        //var_dump(dirname(__FILE__) . '/modules/php/' . implode(DIRECTORY_SEPARATOR, $classParts) . '.php');die;
         $file = dirname(__FILE__) . '/modules/php/' . implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
         if (file_exists($file)) {
             require_once $file;
@@ -17,19 +21,32 @@ spl_autoload_register($swdNamespaceAutoload, true, true);
 require_once( APP_GAMEMODULE_PATH . 'module/table/table.game.php' );
 
 class Desperados extends Table {
+    
+    use ZombieTrait;
 
     /**
      * 
      * @var Desperados
      */
     private static $instance;
-
+    
     public function __construct() {
+        parent::__construct();
+
         self::$instance = $this;
+
+        self::initGameStateLabels(array(
+                //    "my_first_global_variable" => 10,
+                //    "my_second_global_variable" => 11,
+                //      ...
+                //    "my_first_game_variant" => 100,
+                //    "my_second_game_variant" => 101,
+                //      ...
+        ));
     }
 
     protected function getGameName() {
-        return "Desperados";
+        return "desperados";
     }
 
     /*
@@ -41,7 +58,38 @@ class Desperados extends Table {
      */
 
     protected function setupNewGame($players, $options = array()) {
+        // Set the colors of the players with HTML color code
+        // The default below is red/green/blue/orange/brown
+        // The number of colors defined here must correspond to the maximum number of players allowed for the gams
+        $gameinfos = self::getGameinfos();
+        $default_colors = $gameinfos['player_colors'];
+
+        // Create players
+        // Note: if you added some extra field on "player" table in the database (dbmodel.sql), you can initialize it there.
+        $sql = "INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar) VALUES ";
+        $values = array();
+        foreach ($players as $player_id => $player) {
+            $color = array_shift($default_colors);
+            $values[] = "('" . $player_id . "','$color','" . $player['player_canal'] . "','" . addslashes($player['player_name']) . "','" . addslashes($player['player_avatar']) . "')";
+        }
+        $sql .= implode($values, ',');
+        self::DbQuery($sql);
+        self::reattributeColorsBasedOnPreferences($players, $gameinfos['player_colors']);
+        self::reloadPlayersBasicInfos();
+
+        /*         * ********** Start the game initialization **** */
+
+        // Init global values with their initial values
+        //self::setGameStateInitialValue( 'my_first_global_variable', 0 );
+        // Init game statistics
+        // (note: statistics used in this file must be defined in your stats.inc.php file)
+        //self::initStat( 'table', 'table_teststat1', 0 );    // Init a table statistics
+        //self::initStat( 'player', 'player_teststat1', 0 );  // Init a player statistics (for all players)
+        // TODO: setup the initial game situation here
+        // Activate first player (which is in general a good idea :) )
         $this->activeNextPlayer();
+
+        /*         * ********** End of the game initialization **** */
     }
 
     /*
@@ -55,22 +103,78 @@ class Desperados extends Table {
      */
 
     protected function getAllDatas() {
-        return;
+        $result = array();
+
+        $current_player_id = self::getCurrentPlayerId();    // !! We must only return informations visible by this player !!
+        // Get information about players
+        // Note: you can retrieve some extra field you added for "player" table in "dbmodel.sql" if you need it.
+        $sql = "SELECT player_id id, player_score score FROM player ";
+        $result['players'] = self::getCollectionFromDb($sql);
+
+        // TODO: Gather all information about current game situation (visible by player $current_player_id).
+
+        return $result;
     }
+
+    /*
+      getGameProgression:
+
+      Compute and return the current game progression.
+      The number returned must be an integer beween 0 (=the game just started) and
+      100 (= the game is finished or almost finished).
+
+      This method is called each time we are in a game state with the "updateGameProgression" property set to true
+      (see states.inc.php)
+     */
 
     function getGameProgression() {
         // TODO: compute and return the game progression
 
         return 0;
     }
+
+
+
+
     
-    /**
-     * Get curent instance of Desperados Game
-     * @return Desperados
+
+///////////////////////////////////////////////////////////////////////////////////:
+////////// DB upgrade
+//////////
+
+    /*
+      upgradeTableDb:
+
+      You don't have to care about this until your game has been published on BGA.
+      Once your game is on BGA, this method is called everytime the system detects a game running with your old
+      Database scheme.
+      In this case, if you change your Database scheme, you just have to apply the needed changes in order to
+      update the game database and allow the game to continue to run with your new version.
+
      */
 
-    public static function getInstance(): Desperados {
-        return self::$instance;
+    function upgradeTableDb($from_version) {
+        // $from_version is the current version of this game database, in numerical form.
+        // For example, if the game was running with a release of your game named "140430-1345",
+        // $from_version is equal to 1404301345
+        // Example:
+//        if( $from_version <= 1404301345 )
+//        {
+//            // ! important ! Use DBPREFIX_<table_name> for all tables
+//
+//            $sql = "ALTER TABLE DBPREFIX_xxxxxxx ....";
+//            self::applyDbUpgradeToAllDB( $sql );
+//        }
+//        if( $from_version <= 1405061421 )
+//        {
+//            // ! important ! Use DBPREFIX_<table_name> for all tables
+//
+//            $sql = "CREATE TABLE DBPREFIX_xxxxxxx ....";
+//            self::applyDbUpgradeToAllDB( $sql );
+//        }
+//        // Please add your future database scheme changes here
+//
+//
     }
 
 }
